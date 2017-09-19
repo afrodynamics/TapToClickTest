c7d0bda29385062da4e80a138717ea9d34494387
Dexter Friedman <iamtheonlydexter@yahoo.com>
2017-09-19 14:18:07 -0700

    TapToClick example, working on OS X with VS for Mac

---
+++ TapToClickTest/TapToClickTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TapToClickTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.MacOSX) {
+				string env = Environment.GetEnvironmentVariable("DYLD_LIBRARY_PATH");
+                Environment.SetEnvironmentVariable("DYLD_LIBRARY_PATH", env + ":./osx");
+            }
+
+            using (var g = new TestGame()) {
+                g.Run();
+            }
+        }
+    }
+}
---
+++ TapToClickTest/TapToClickTest/TestGame.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TapToClickTest
+{
+    public class TestGame : Game
+    {
+
+        GraphicsDeviceManager gdm;
+        SpriteBatch sb;
+        List<LameSprite> spriteList;
+        static Texture2D whiteTex;
+
+        class LameSprite {
+
+            int x, y;
+            Color color = Color.Red;
+            public bool dead = false;
+
+            public LameSprite(int x, int y, Color c) {
+                this.x = x;
+                this.y = y;
+                color = c;
+            }
+
+            public void Draw(SpriteBatch batch) {
+                if (dead)
+                    return;
+
+                batch.Draw(whiteTex, new Rectangle(x - 5, y - 5, 10, 10), null, color);
+            }
+        }
+
+        public TestGame()
+        {
+			gdm = new GraphicsDeviceManager(this);
+            spriteList = new List<LameSprite>();
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            IsMouseVisible = true;
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            sb = new SpriteBatch(GraphicsDevice);
+            whiteTex = new Texture2D(GraphicsDevice, 1, 1);
+            whiteTex.SetData<Color>(new Color[] {
+                Color.White
+            });
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+
+            whiteTex.Dispose();
+        }
+
+        private MouseState prevState;
+        private MouseState nextState;
+
+        protected override void Update(GameTime gameTime)
+        {
+            float dt = gameTime.ElapsedGameTime.Ticks / TimeSpan.TicksPerSecond;
+
+            base.Update(gameTime);
+
+            // Swap mouse states
+            prevState = nextState;
+            nextState = Mouse.GetState();
+
+            if (prevState == null)
+                return;
+
+            if (nextState.LeftButton == ButtonState.Pressed && prevState.LeftButton == ButtonState.Released) {
+                spriteList.Add(new LameSprite(nextState.X, nextState.Y, Color.Purple));
+            }
+            else if (nextState.LeftButton == ButtonState.Pressed) 
+            {
+                spriteList.Add(new LameSprite(nextState.X, nextState.Y, Color.Red));
+            }
+
+            var kbState = Keyboard.GetState();
+
+            if (kbState.IsKeyDown(Keys.Space)) {
+                spriteList.Clear();
+            }
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            GraphicsDevice.Clear(Color.White);
+
+			var removeList = new List<LameSprite>();
+
+            foreach (var s in spriteList) {
+                sb.Begin();
+                s.Draw(sb);
+				sb.End();
+            }
+        }
+    }
+}
